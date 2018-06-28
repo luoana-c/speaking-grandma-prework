52fa8443cd12eeef8a0a97fdf67a3a9ce273a658
Luoana Chirita <luoana.livadariu@gmail.com>
2018-06-28 11:42:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  end
   
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  end
   
   else 
     return "HUH? SPEAK UP, SONNY!"
